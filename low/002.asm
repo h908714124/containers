73b162e901e981c43e2c18d71ec80e3bc7a409ee
jbock <jbock-java@gmx.de>
2024-04-01 19:55:57 +0200

    hello intel x86_64

--- low/002.asm
+++ low/002.asm
@@ -1,18 +1,19 @@
 .global _start
 .intel_syntax
+
+.section .data
+	msg: .ascii "ABC\n"
+
 .section .text
 
 _start:
-	mov dword ptr [%esp], 0x0a434241
-	mov %eax, 4
-	mov %ebx, 1
-	mov %ecx, %esp
-	mov %edx, 4
-	int 0x80
+	mov %rax, 1
+	mov %rdi, 1
+	mov %rsi, offset msg
+	mov %rdx, 4
+	syscall
 	#exit syscall
-	mov %eax, 1
-	mov %ebx, 65
-	int 0x80
+	mov %rax, 60
+	mov %rdi, 65
+	syscall
 
-.section .data
-	message:
